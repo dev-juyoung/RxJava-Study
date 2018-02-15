6f662ba40f2e12c3743329a672f7c94336f1b599
dev.juyoung <dev.juyoung@gmail.com>
2018-02-16 00:48:16 +0900

    * 주석 추가.

--- src/main/java/chapter2/ObservableCreate.java
+++ src/main/java/chapter2/ObservableCreate.java
@@ -3,6 +3,10 @@ package chapter2;
 import io.reactivex.Observable;
 
 public class ObservableCreate {
+    /**
+     * just()와는 다르게,
+     * onNext(), onError(), onComplete() 를 개발자가 직접 구현해주어야 함.
+     */
     private Observable<Integer> dataSource = Observable.create(
             emitter -> {
                 emitter.onNext(100);
@@ -13,6 +17,7 @@ public class ObservableCreate {
     );
 
     public void emit() {
+        // subscribe()를 호출하지 않으면, 구독을 하지 않으므로, 데이터를 만들어두고 실제 발행은 하지 않음.
         dataSource.subscribe(System.out::println);
     }
 
