3a95f056b5d1e4a068344b40c3496bf891e8805b
dev.juyoung <dev.juyoung@gmail.com>
2018-02-19 21:29:48 +0900

    * ConnectableObservable 샘플 작성.

---
+++ src/main/java/chapter2/ConnectableObservableSample.java
@@ -0,0 +1,38 @@
+package chapter2;
+
+import io.reactivex.Observable;
+import io.reactivex.observables.ConnectableObservable;
+
+import java.util.concurrent.TimeUnit;
+
+public class ConnectableObservableSample {
+
+    public void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void emit() {
+        String[] dataSet = { "RED", "GREEN", "BLUE" };
+        Observable<String> observable = Observable.interval(100L, TimeUnit.MILLISECONDS)
+                .map(Long::intValue)
+                .map(i -> dataSet[i])
+                .take(dataSet.length);
+
+        ConnectableObservable<String> dataSource = observable.publish();
+        dataSource.subscribe(data -> System.out.println("Subscriber#1 => " + data));
+        dataSource.subscribe(data -> System.out.println("Subscriber#2 => " + data));
+        dataSource.connect();
+        sleep(250);
+        dataSource.subscribe(data -> System.out.println("Subscriber#3 => " + data));
+        sleep(150);
+    }
+
+    public static void main(String[] args) {
+        ConnectableObservableSample instance = new ConnectableObservableSample();
+        instance.emit();
+    }
+}
